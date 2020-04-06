714db0f4223113d4182da5e6082389c9eb165abd
Connor Berry <cdb979@gmail.com>
2020-04-05 22:14:41 -0400

    Got files right here, we're good

--- Driver.java
+++ Driver.java
@@ -1,20 +1,40 @@
 import javax.swing.*;
+import java.awt.event.*;
+import java.io.File;
+import java.util.ArrayList;
 
-public class Driver{
+public class Driver {
     public static void main(String [] args){
+        ArrayList<File> files = new ArrayList<File>();
+
         JFrame frame = new JFrame();
 
         JButton fileChooseBtn = new JButton("Choose Files");
         JButton constructBtn = new JButton("Construct Inverted Indices");
-        
-        fileChooseBtn.setBounds(250, 250, 100, 50);
-        constructBtn.setBounds(100, 250, 100, 50);
+        int btnWidth = 250;
+        fileChooseBtn.setBounds(250 - btnWidth/2, 200, btnWidth, 50);
+        constructBtn.setBounds(250 - btnWidth/2, 300, btnWidth, 50);
+
+        fileChooseBtn.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                JFileChooser fc = new JFileChooser();
+                fc.setMultiSelectionEnabled(true);
+                int returnVal = fc.showOpenDialog(frame);
+                if (returnVal == JFileChooser.APPROVE_OPTION){
+                    File [] get = fc.getSelectedFiles();
+                    for(int i = 0; i < get.length; i++){
+                        files.add(get[i]);
+                    }
+                }
+            }
+        });
         
         frame.add(fileChooseBtn);
         frame.add(constructBtn);
 
         frame.setSize(500, 500);
         frame.setLayout(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
     }
 }
