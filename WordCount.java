64de26dc00cc1efe2f79d16d7e43f9b3e8dcdc59
Connor Berry <cdb979@gmail.com>
2020-04-08 15:34:42 -0400

    Merged into one class

--- WordCount.java
+++ WordCount.java
@@ -1,21 +1,74 @@
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.StringTokenizer;
 
-import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.FileSplit;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.conf.*;
 import org.apache.hadoop.mapreduce.*;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.mapreduce.Reducer;
 
-public class WordCount 
+public class WordCount {
+    static class WordCountReducer extends Reducer<Text, IntWritable, Text, IntWritable>
 {
-    public static void main(String[] args)
-        throws IOException, ClassNotFoundException, InterruptedException {
+    public void reduce(Text key, Iterable<Text> docs, Context context)
+        throws IOException, InterruptedException
+    {
+        HashMap<Text, Integer> docCount = new HashMap<Text, Integer>();
+
+        for (Text docId : docs) {
+            if(docCount.containsKey(docId)){
+                Integer x = docCount.get(docId);
+                docCount.put(docId, x+1);
+            }else{
+                docCount.put(docId, 1);
+            }
+        }
+
+        docCount.forEach((Text k, Integer v) -> {
+            try{
+                context.write(k, new IntWritable(v));
+            }
+            catch (Exception e){
+                System.out.println("An error occurred in the context writting portion of the reducer.");
+            }
+        });
+    }
+    }
+
+    static class WordCountMapper extends Mapper<LongWritable, Text, Text, Text>
+    {
+        /**
+         * @param args the command line arguments
+         */
+        private Text word = new Text();
+
+        public void map(LongWritable key, Text value, Context context)
+            throws IOException, InterruptedException
+        {
+            String line = value.toString();
+            StringTokenizer tokenizer = new StringTokenizer(line);
+
+            while (tokenizer.hasMoreTokens()) {
+                word.set(tokenizer.nextToken());
+                // to the context, write an instance of this word : document key value pair
+                context.write(word, new Text(((FileSplit)context.getInputSplit()).getPath().toString()));
+            }
+        }
+    }
+    public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
         if (args.length != 2) {
             System.err.println("Usage: java WordCount <input path> <output path>");
             System.exit(-1);
         }
 
-        Job job = new Job();
+        Job job = new Job(new Configuration(), "wordcount");
         job.setJarByClass(WordCount.class);
         job.setJobName("WordCount");
 
--- makefile
+++ makefile
@@ -3,7 +3,5 @@ CP=".:./lib/*"
 client: Client.java
 	javac Client.java
 
-wc: WordCount.java WordCountMapper.java WordCountReducer.java
-	javac -cp $(CP) WordCountMapper.java
-	javac -cp $(CP) WordCountReducer.java
+wc: WordCount.java
 	javac -cp $(CP) WordCount.java
