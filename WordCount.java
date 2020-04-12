ab8a8a2160fbcfa551cebe09755373d3c167cb71
Connor Berry <cdb979@gmail.com>
2020-04-12 17:46:24 -0400

    Edited wordcount in an attempt to fix duplication errors

--- WordCount.java
+++ WordCount.java
@@ -20,16 +20,26 @@ public class WordCount {
 			/* We will store a map of words : wordcount in this hashmap */
 			HashMap<Text, Integer> docCount = new HashMap<Text, Integer>();
 			for (Text docId : docs) {
-				/* Either we will update an already existing document ID in the hashmap, or create a new one with
-				with just '1' as the value */
-				docCount.put(docId, (docCount.containsKey(docId) ? docCount.get(docId) + 1 : 1));
+				Text k = new Text(key.toString() + "\t" + docId.toString());
+				/*
+				 * Either we will update an already existing document ID in the hashmap, or
+				 * create a new one with with just '1' as the value
+				 */
+				if (docCount.containsKey(k)) {
+					Integer x = docCount.get(k);
+					docCount.put(k, x + 1);
+				} else {
+					docCount.put(k, 1);
+				}
 			}
 
-			String strKey = key.toString()+"\t";
-			/* For each key : value of documentId : count, we will write (word, documentId, count) to the context */
+			/*
+			 * For each key : value of documentId : count, we will write (word, documentId,
+			 * count) to the context
+			 */
 			docCount.forEach((Text k, Integer v) -> {
 				try {
-					context.write(new Text(strKey+k), new IntWritable(v));
+					context.write(k, new IntWritable(v));
 				} catch (Exception e) {
 					System.out.println("An error occurred in the context writting portion of the reducer.");
 				}
@@ -45,16 +55,24 @@ public class WordCount {
 
 		public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
 			String line = value.toString().toLowerCase();
-			/* Remove punctionation from line before tokenizing, so that single-term tokens don't get clobbered */
+			/*
+			 * Remove punctionation from line before tokenizing, so that single-term tokens
+			 * don't get clobbered
+			 */
 			line = line.replaceAll("\\p{Punct}", "");
 			StringTokenizer tokenizer = new StringTokenizer(line);
 
-			/* Retreive file name to use as docId, we do this here so that we don't contantly overload I/O */
+			/*
+			 * Retreive file name to use as docId, we do this here so that we don't
+			 * contantly overload I/O
+			 */
 			Text docId = new Text(((FileSplit) context.getInputSplit()).getPath().getName());
 
 			while (tokenizer.hasMoreTokens()) {
 				word.set(tokenizer.nextToken());
-				/* Write a pair of word : document to the context, which will be reduced later */
+				/*
+				 * Write a pair of word : document to the context, which will be reduced later
+				 */
 				context.write(word, docId);
 			}
 		}
